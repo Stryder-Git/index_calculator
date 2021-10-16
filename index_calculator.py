82425e3c27adc9fbd3962fa55252fcd269c4b0aa
Stryder-Git <marcel_pieper@hotmail.com>
2021-10-16 14:55:41 -0400

    made it cache the timeseries (2) smaller adjustments (3) created readme.md

--- index_calculator.py
+++ index_calculator.py
@@ -414,16 +414,18 @@ class IndexCalculator:
         self.align = any(self._aligns)
 
         if schedule is None:
+            self.schedtz = self.SCHEDTZ
             self.schedule = pd.DataFrame(columns=["real", "start", "end", "session"])
-            self._sched = self.schedule
             self.__frm = self._some_date
             self.__to = self._some_date
         else:
+            self.schedtz = schedule.iloc[:, 0].dt.tz
             self._verify_schedule(schedule)
             self.schedule = self._create_sessions_and_parts(schedule)
-            self._sched = self.schedule
             self.__frm = self.schedule.real.iat[0]
             self.__to = self.schedule.end.iat[-1]
+
+        self.__times = None
 
     use = __init__
 
@@ -432,6 +434,7 @@ class IndexCalculator:
                 pre=False, brk=False, rth=False, post=False):
         original_settings = self.settings
         original_schedule = self.schedule.copy()  # keeping it seperate from settings to avoid recalculation
+        original_tz = self.schedtz
         frm = self.__frm
         to = self.__to
         self.use(schedule=schedule, frequency=frequency, start=start,
@@ -439,7 +442,7 @@ class IndexCalculator:
         yield
         self.use(schedule=None, **original_settings)  # passing None and setting it directly,
         self.schedule = original_schedule  # bypasses the calculation
-        self._sched = original_schedule
+        self.schedtz = original_tz
         self.__frm = frm
         self.__to = to
 
@@ -461,6 +464,7 @@ class IndexCalculator:
 
     @frequency.setter
     def frequency(self, frequency):
+        self.__times = None
         self._freq = pd.Timedelta(frequency)
         if not "_change" in self.schedule:
             # _change is left in there if the start column has never been calculated
@@ -527,21 +531,20 @@ class IndexCalculator:
         return schedule
 
     @contextmanager
-    def _temp(self, frequency, frm, to):
-        """frm and to can never be None, but frequency can"""
-        _sched = self.schedule.real.ge(frm.normalize()) & self.schedule.end.lt(to.normalize() + self._day)
+    def _temp(self, frequency):
+        """temporarily reset the configured frequency"""
         if frequency is None:
-            self._sched = self.schedule[_sched]
-            yield;
-            return
+            yield; return
 
         original_freq = self._freq
-        original_start = self.schedule.start.copy()
+        original_start = self.schedule.start
+        original_times = self.__times
+        self.__times = None
         self.frequency = frequency  # will adjust starts
-        self._sched = self.schedule[_sched]
         yield
         self.schedule["start"] = original_start  # reset to prior
         self._freq = original_freq
+        self.__times = original_times
 
     def _handle_start_end(self, ts):
         """This will adjust the calculated time_series (column: 'target' in ts) according to the
@@ -574,19 +577,26 @@ class IndexCalculator:
         time_series.name = None
         return time_series.drop_duplicates().reset_index(drop=True)
 
-    def _times(self):
+    def _times(self, frm, to):
         """Method used by date_range to calculate the trading index.
          :return: pd.Series of datetime64[ns, UTC]"""
-        ts = np.ceil((self._sched.end - self._sched.start) / self.frequency)
-        ts = self._sched.index.repeat(ts)
-        ts = self._sched.reindex(ts).reset_index(drop=True)
 
-        ts["target"] = (ts.start.groupby(ts.real).cumcount() + 1) * self.frequency + ts.start
-        if self._adj:
-            return self._handle_start_end(ts).sort_values().reset_index(drop=True)
-        else:
-            ts.target.name = None
-            return ts.target.reset_index(drop=True)
+        if self.__times is None:
+            ts = np.ceil((self.schedule.end - self.schedule.start) / self.frequency)
+            ts = self.schedule.index.repeat(ts)
+            ts = self.schedule.reindex(ts).reset_index(drop=True)
+
+            ts["target"] = (ts.start.groupby(ts.real).cumcount() + 1) * self.frequency + ts.start
+            if self._adj:
+                self.__times = self._handle_start_end(ts).sort_values().reset_index(drop=True)
+            else:
+                ts.target.name = None
+                self.__times = ts.target.reset_index(drop=True)
+
+        return self.__times[self.__times.ge(frm.normalize()) &
+                            self.__times.le(to.normalize() + self._day)]
+
+    def clear(self): self.__times = None
 
     def times(self, frequency=None, frm=None, to=None, tz=None):
         """
@@ -602,53 +612,46 @@ class IndexCalculator:
                 frm = self.__frm
             else:
                 frm = pd.Timestamp(frm)
-                try:
-                    frm = frm.tz_localize(self.SCHEDTZ)
-                except TypeError:
-                    pass
+                try: frm = frm.tz_localize(self.schedtz)
+                except TypeError: pass
 
             if to is None:
                 to = self.__to
             else:
                 to = pd.Timestamp(to)
-                try:
-                    to = to.tz_localize(self.SCHEDTZ)
-                except TypeError:
-                    pass
+                try: to = to.tz_localize(self.schedtz)
+                except TypeError: pass
 
-            with self._temp(frequency, frm, to):
-                return self._times()
+            with self._temp(frequency):
+                return self._times(frm, to)
 
         else:
             if frm is None:
                 frm = self.__frm
             else:
                 frm = pd.Timestamp(frm)
-                try:
-                    frm = frm.tz_localize(tz).tz_convert(self.SCHEDTZ)
-                except TypeError:
-                    frm = frm.tz_convert(self.SCHEDTZ)
+                try: frm = frm.tz_localize(tz).tz_convert(self.schedtz)
+                except TypeError: frm = frm.tz_convert(self.schedtz)
 
             if to is None:
                 to = self.__to
             else:
                 to = pd.Timestamp(to)
-                try:
-                    to = to.tz_localize(tz).tz_convert(self.SCHEDTZ)
-                except TypeError:
-                    to = to.tz_convert(self.SCHEDTZ)
+                try: to = to.tz_localize(tz).tz_convert(self.schedtz)
+                except TypeError: to = to.tz_convert(self.schedtz)
 
-            with self._temp(frequency, frm, to):
-                return self._times().dt.tz_convert(tz)
+            with self._temp(frequency):
+                return self._times(frm, to).dt.tz_convert(tz)
 
-    def timex(self, frequency=None, frm=None, to=None, tz=SCHEDTZ):
+
+    def timex(self, frequency=None, frm=None, to=None, tz=None):
         return pd.DatetimeIndex(self.times(frequency=frequency, frm=frm, to=to, tz=tz))
 
     def __call__(self, schedule, frequency, start=True, end=False,
                  pre=False, brk=False, rth=False, post=False):
         with self.use_now(schedule, frequency, start=start, end=end,
                           pre=pre, brk=brk, rth=rth, post=post):
-            return pd.DatetimeIndex(self._times())
+            return pd.DatetimeIndex(self._times(self.__frm, self.__to))
 
     __call__.__doc__ = __init__.__doc__
 
@@ -663,6 +666,7 @@ class IndexCalculator:
         dmax += self.__inferred_timeframe
         if dmin < self.__frm or dmax > self.__to: raise InvalidInput("Schedule doesn't cover the whole data")
 
+        # _sched is needed by the helper functions
         self._sched = self.schedule[self.schedule.real.ge(dmin) & self.schedule.end.le(dmax)]
         if not self._sched.real.isin(data.index).all():
             raise InvalidInput("You seem to have missing data. This is not refering to NaN values but to "
@@ -722,10 +726,10 @@ class IndexCalculator:
         """
         origins = self._sched.loc[self._sched.real.isin(firstrows)]
         origins = origins["start"].set_axis(origins["real"])
-        frequency = self.frequency  # to avoid going through @property every time
+        f = self.frequency
 
         def __new(df):
-            return df.resample(frequency, origin=origins.at[df.index[0]], label=label
+            return df.resample(f, origin=origins.at[df.index[0]], label=label
                                ).agg(agg_map)
 
         return __new
@@ -750,7 +754,7 @@ class IndexCalculator:
 
         if self.align:
             assert not self._srt in data, f"Please rename column: {self._srt}."
-            data[self._srt] = np.arange(data.shape[0])
+            data[self._srt] = np.arange(data.shape[0]) # a column with integers showing the original order of the parts
             agg_map[self._srt] = "first"
             parts = []
             if even:
@@ -767,7 +771,7 @@ class IndexCalculator:
                                     ).dropna(how="any"))
 
             new = pd.concat(parts).sort_values(self._srt)
-            new = new.set_index(pd.DatetimeIndex(self._times()), drop=True
+            new = new.set_index(pd.DatetimeIndex(self._times(self.__frm, self.__to)), drop=True
                                 ).drop(columns=self._srt)
         elif even:
             new = data.resample(self.frequency, label=label, origin=self._sched.start.iat[0]
@@ -800,13 +804,13 @@ class IndexCalculator:
 
         try:
             _tz = data.index.tz
-            data = data.tz_convert(self.SCHEDTZ)
+            data = data.tz_convert(self.schedtz)
         except TypeError as e:
             if tz is None:
                 raise TimeZoneException("When the index is tz-naive, you must pass"
                                         " the tz that it should be interpreted in") from e
             is_aware = False
-            data = data.tz_localize(tz).tz_convert(self.SCHEDTZ)
+            data = data.tz_localize(tz).tz_convert(self.schedtz)
         else:
             is_aware = True
             tz = _tz
@@ -817,13 +821,11 @@ class IndexCalculator:
             agg_map = self.default_agg_map
             data.columns = data.columns.str.lower()
 
-        with self._temp(freq, self.__frm, self.__to):
+        with self._temp(freq):
             new = self._convert(data, agg_map, closed).tz_convert(tz)
 
-        if is_aware:
-            return new
-        else:
-            return new.tz_localize(None)
+        if is_aware: return new
+        return new.tz_localize(None)
 
 
 class IndexCalculatorException(ValueError):
---
+++ readme.md
@@ -0,0 +1,15 @@
+##CAUTION
+
+The IndexCalculator class started as a solution to a specific personal use and ended up growing into something that 
+I just wanted to create. It allows a lot of flexibility when calculating DatetimeIndexes and converting standard 
+pricedata but, except for my own usage, a lot is still untested.
+
+
+What do I want to change?
+
+    unless a different frequency is specified, I want to return a cached timeseries, subsetting by frm and to
+
+
+
+
+
